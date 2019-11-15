ba921839e53ac0cb1639bce932c92fb53b6e3a0a
GeVhoo <gvahrushev@mail.ru>
2019-11-15 15:54:38 +0500

    Ninth step

--- brain_games/scripts/games/brain_prime.py
+++ brain_games/scripts/games/brain_prime.py
@@ -20,38 +20,23 @@ def brain_prime():
     print()
     index = 1
     while index <= 3:
-        step = randint(1, 10)
-        n1 = randint(1, 10)
-        n2 = n1 + step
-        n3 = n2 + step
-        n4 = n3 + step
-        n5 = n4 + step
-        n6 = n5 + step
-        n7 = n6 + step
-        n8 = n7 + step
-        n9 = n8 + step
-        n10 = n9 + step
-        list_n = (n1, n2, n3, n4, n5, n6, n7, n8, n9, n10)
-        random_n = choice(list_n)
-        mask = []
-        for item in list_n:
-            if item == random_n:
-                mask.append('..')
-            elif item != random_n:
-                mask.append(item)
-        mask = str(mask)
-        list_char = "[],'"
-        for char in list_char:
-            mask = mask.replace(char, '')
-        print('Question: {}'.format(mask))
+        number = randint(2, 100)
+        d = 2
+        while number % d != 0:
+            d += 1
+        if d != number:
+            correct_answer = 'no'
+        if d == number:
+            correct_answer = 'yes'
+        print('Question: {}'.format(number))
         your_answer = prompt.string('Your answer: ')
-        if your_answer == str(random_n):
+        if your_answer == correct_answer:
             print(Fore.GREEN + 'Correct!')
             index += 1
         else:
             return print(Fore.RED +
                     "'{}' is wrong answer ;(. Correct answer was '{}'."
-                    "\nLet\'s try again, {}!".format(your_answer, random_n, name))
+                    "\nLet\'s try again, {}!".format(your_answer, correct_answer, name))
     print(Fore.BLUE + Style.BRIGHT + 'Congratulations, {}!'.format(name))
 
 
